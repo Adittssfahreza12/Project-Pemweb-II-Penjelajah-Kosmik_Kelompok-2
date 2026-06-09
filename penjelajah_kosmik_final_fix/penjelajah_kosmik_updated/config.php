2b0c65ace7f861eec95c95baaf0cef2c4c7ce84c
Parida Lubis <paridalubis2022tt@gmail.com>
2026-06-09 10:57:00 +0700

    Delete penjelajah_kosmik_final_fix/penjelajah_kosmik_updated directory

--- penjelajah_kosmik_final_fix/penjelajah_kosmik_updated/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_host = 'localhost';
-$db_user = 'root';
-$db_pass = '';
-$db_name = 'penjelajah_kosmik';
-
-// Koneksi digunakan langsung di auth_api.php dan halaman lain yang butuh DB
-// Tidak di-include di sini agar tidak ada koneksi ganda
-?>
--- penjelajah_kosmik_final_fix/penjelajah_kosmik_updated/navbar.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('navHamburger');
-const navLinks  = document.querySelector('.navbar-links');
-
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('open');
-});
